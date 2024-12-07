5649608e5b1c12bedffb6bfd6adfee1f76bc8a5f
Devendra Tiwari <dtiwari18@gmail.com>
2024-12-08 04:16:58 +0530

    [Capstone-16] Connect to MongoDB and Redis

---
+++ cartservice/src/main/java/com/scaler/cartservice/configs/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.scaler.cartservice.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
+}
